fc3b7f581f0fe95e03a1833e915690f4c247dd94
Husein <70703161+AkbarHusein@users.noreply.github.com>
2022-01-08 11:25:28 +0700

    Update db_stock_barang.sql
    update script

--- db_stock_barang.sql
+++ db_stock_barang.sql
@@ -1,26 +1,161 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               5.7.33 - MySQL Community Server (GPL)
--- Server OS:                    Win64
--- HeidiSQL Version:             11.3.0.6295
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jan 08, 2022 at 04:25 AM
+-- Server version: 5.7.33
+-- PHP Version: 7.4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Data exporting was unselected.
+--
+-- Database: `db_stock_barang`
+--
 
--- Data exporting was unselected.
+-- --------------------------------------------------------
 
--- Data exporting was unselected.
+--
+-- Table structure for table `tb_barang`
+--
 
--- Data exporting was unselected.
+CREATE TABLE `tb_barang` (
+  `kode_barang` varchar(10) NOT NULL,
+  `nama_barang` varchar(20) NOT NULL,
+  `pemasok` varchar(25) NOT NULL,
+  `tanggal` varchar(50) NOT NULL,
+  `kategori` varchar(25) NOT NULL,
+  `jumlah` int(11) NOT NULL DEFAULT '0',
+  `keterangan` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+--
+-- Dumping data for table `tb_barang`
+--
+
+INSERT INTO `tb_barang` (`kode_barang`, `nama_barang`, `pemasok`, `tanggal`, `kategori`, `jumlah`, `keterangan`) VALUES
+('bks', 'mouse', 'husein', '12-01-2022', '', 6, 'kepo'),
+('BR', 'Keyboard', 'Alamanda', '08-01-2022', 'Elektronik', 9, 'Barang Import');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_kategori`
+--
+
+CREATE TABLE `tb_kategori` (
+  `kode_kategori` varchar(50) NOT NULL,
+  `tanggal` varchar(50) DEFAULT NULL,
+  `nama_kategori` varchar(50) NOT NULL,
+  `keterangan` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_kategori`
+--
+
+INSERT INTO `tb_kategori` (`kode_kategori`, `tanggal`, `nama_kategori`, `keterangan`) VALUES
+('BKS', '04-01-2022', 'Laptop', 'Barang Bekas'),
+('BR', '05-01-2022', 'TV', 'Barang Baru'),
+('BR', '04-01-2022', 'Kasur', 'Barang Baru');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_supplier`
+--
+
+CREATE TABLE `tb_supplier` (
+  `kode` int(11) NOT NULL,
+  `nama` varchar(25) NOT NULL,
+  `nohp` varchar(13) NOT NULL,
+  `alamat` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_supplier`
+--
+
+INSERT INTO `tb_supplier` (`kode`, `nama`, `nohp`, `alamat`) VALUES
+(5, 'Alamanda', '023333333', 'Tanggerang'),
+(6, 'Zain', '0812312345', 'Jl. Kurdi'),
+(7, 'Wira', '0823768689', 'Jl.  Batam'),
+(8, '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `nohp` varchar(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`id`, `username`, `password`, `email`, `nohp`) VALUES
+(1, 'admin', 'admin', 'admin@gmail.com', '08123456789');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  ADD UNIQUE KEY `kode_barang` (`kode_barang`);
+
+--
+-- Indexes for table `tb_kategori`
+--
+ALTER TABLE `tb_kategori`
+  ADD KEY `kode_kategori` (`kode_kategori`);
+
+--
+-- Indexes for table `tb_supplier`
+--
+ALTER TABLE `tb_supplier`
+  ADD PRIMARY KEY (`kode`);
+
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_supplier`
+--
+ALTER TABLE `tb_supplier`
+  MODIFY `kode` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
